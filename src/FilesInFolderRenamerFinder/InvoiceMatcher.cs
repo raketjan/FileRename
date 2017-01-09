9161f146f4f1157405f86fc8c51f7a978fad890a
DESKTOP-QKO70II\Jan Nordberg <raketjan@gmail.com>
2017-01-09 17:21:30 +0100

    funktionalitet för att matcha underfakturor med fakturor för Bo jul 2016

---
+++ src/FilesInFolderRenamerFinder/InvoiceMatcher.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilesInFolderRenamerFinder
+{
+    public class InvoiceMatcher
+    {
+        private Dictionary<string, string> DelFakturaTillUnderFaktura = new Dictionary<string, string>();
+
+
+
+        public void BuildDictionary(string filesFolder)
+        {
+            DirectoryInfo d = new DirectoryInfo(filesFolder);
+            FileInfo[] Files = d.GetFiles();
+
+            foreach (var file in Files)
+            {
+                string SamlingsFaktura = file.Name.Split('.')[0];
+                using (StreamReader sr = file.OpenText())
+                {
+                    string UnderFaktura = "";
+                    while ((UnderFaktura = sr.ReadLine()) != null)
+                    {
+                        AddToDictionary(UnderFaktura, SamlingsFaktura);
+                    }
+                }
+
+            }
+        }
+
+        private void AddToDictionary(string underFaktura, string samlingsFaktura)
+        {
+            if (!DelFakturaTillUnderFaktura.ContainsKey(underFaktura))
+                DelFakturaTillUnderFaktura.Add(underFaktura, samlingsFaktura);
+            else if (DelFakturaTillUnderFaktura[underFaktura].Contains(samlingsFaktura))
+                ;       // duplicate in file
+            else
+            {
+                DelFakturaTillUnderFaktura[underFaktura] += ", " + samlingsFaktura;
+            }
+        }
+
+        public void MatchAndWriteFile(string columnsFile, string outputFile)
+        {
+            List<string> matchings = new List<string>();
+            int notfoundCount = 0;
+
+            foreach (string underFaktura in File.ReadAllLines(columnsFile))
+            {
+                string matchning = "";
+                if (DelFakturaTillUnderFaktura.ContainsKey(underFaktura))
+                    matchning = DelFakturaTillUnderFaktura[underFaktura];
+                else
+                {
+                    notfoundCount++;
+                }
+                matchings.Add(underFaktura + ";" + matchning);
+            }
+
+            File.WriteAllLines(outputFile, matchings);
+        }
+    }
+}
--- src/FilesInFolderRenamerFinder/Program.cs
+++ src/FilesInFolderRenamerFinder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,20 +10,32 @@ namespace FilesInFolderRenamerFinder
 {
     public class Program
     {
-        [STAThread]
+        //[STAThread]
+        //public static void Main(string[] args)
+        //{
+        //    int numberofprefixdigits = 3;
+        //    string startfolder = FileRenamer.GetInputFolder();
+        //    string resultfolder = FileRenamer.GetOutputFolder();
+        //    DirectoryInfo d = new DirectoryInfo(resultfolder);
+        //    foreach (DirectoryInfo dir in d.GetDirectories())
+        //    {
+        //        string fakturanummerfile = dir.FullName + "\\fakturor.txt";
+        //        List<FileNameInfo> order = FileRenamer.GetFileOrder(FileRenamer.ReadFakturorFile(fakturanummerfile), 2);
+
+        //        FileRenamer.RenameFiles(startfolder, dir.FullName, order, numberofprefixdigits);
+        //    }
+        //}
+
+
+        // Skapa dictionary över alla 
         public static void Main(string[] args)
         {
-            int numberofprefixdigits = 3;
-            string startfolder = FileRenamer.GetInputFolder();
-            string resultfolder = FileRenamer.GetOutputFolder();
-            DirectoryInfo d = new DirectoryInfo(resultfolder);
-            foreach (DirectoryInfo dir in d.GetDirectories())
-            {
-                string fakturanummerfile = dir.FullName + "\\fakturor.txt";
-                List<FileNameInfo> order = FileRenamer.GetFileOrder(FileRenamer.ReadFakturorFile(fakturanummerfile), 2);
-
-                FileRenamer.RenameFiles(startfolder, dir.FullName, order, numberofprefixdigits);
-            }
+            string FilesFolder = "C:\\Users\\Jan Nordberg\\Desktop\\BoJobb\\410xxxxxPer902xxxxx\\";
+            InvoiceMatcher invoiceMatcher = new InvoiceMatcher();
+            invoiceMatcher.BuildDictionary(FilesFolder);
+            string ColumnsFile = "C:\\Users\\Jan Nordberg\\Desktop\\BoJobb\\20161128 13297 Fortums bilaga till S1 11-1, till excel av BN, sorterad på verifikationsnummer endast kolumn med underfakturenummer.txt";
+            string Outputfile = "C:\\Users\\Jan Nordberg\\Desktop\\BoJobb\\20161128 13297 Fortums bilaga till S1 11-1, till excel av BN, sorterad på verifikationsnummer endast kolumn med underfakturenummer matchad.csv";
+            invoiceMatcher.MatchAndWriteFile(ColumnsFile, Outputfile);
         }
     }
 }
